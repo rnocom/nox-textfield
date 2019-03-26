85c9e67e364a9c1a83caa777ed2d37d36390fdfd
rnocom <nox@adobo.io>
2019-03-26 17:40:45 +0800

    Fixed tests

---
+++ src/components/nox-textfield/nox-textfield.e2e.ts
@@ -0,0 +1,33 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('nox-textfield', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<nox-textfield></nox-textfield>');
+    const element = await page.find('nox-textfield');
+    expect(element).toHaveClass('hydrated');
+  });
+
+  it('renders changes to the name data', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<nox-textfield></nox-textfield>');
+    const component = await page.find('nox-textfield');
+    const element = await page.find('nox-textfield >>> div');
+    expect(element.textContent).toEqual(`Password`);
+/*
+    component.setProperty('first', 'James');
+    await page.waitForChanges();
+    expect(element.textContent).toEqual(`Hello, World! I'm James`);
+
+    component.setProperty('last', 'Quincy');
+    await page.waitForChanges();
+    expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
+
+    component.setProperty('middle', 'Earl');
+    await page.waitForChanges();
+    expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
+    */
+  });
+});
